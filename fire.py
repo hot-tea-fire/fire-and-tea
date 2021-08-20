3bffddd378f0ca9143eb259dc8652c49c7af1a94
redbeanmilktea <1455491990@qq.com>
2021-08-20 18:33:22 +0800

    feat read the first picture
    feat:read the first picture

--- fire.py
+++ fire.py
@@ -1,2 +1,11 @@
+import numpy as np
+import cv2
+
+def main():
+    img = cv2.imread('picture/fire.jpg')
+    print(np.shape(img))
+
+
 if __name__ == '__main__':
     print("Hello world!")
+    main()
