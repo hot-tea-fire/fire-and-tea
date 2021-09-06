886618f71e39d54672996a061003fb1af603270d
redbeanmilktea <1455491990@qq.com>
2021-09-06 17:54:20 +0800

    Revert "feat revert test"
    This reverts commit 1b787fb3598e6323253fd6e9132591cbee92f9b2.

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
 print(236236)
 
 if __name__ == '__main__':
-    print('hello world')
     while 1:
         print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
         time.sleep(2)
-    print('print hello world again')
